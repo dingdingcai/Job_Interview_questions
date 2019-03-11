74d39c7c5d037f7bda31c9377260b9ee5d665bc1
Dingding <caidingding815@foxmail.com>
2019-03-11 13:40:19 +0800

    Update format_transform.py

--- format_transform.py
+++ format_transform.py
@@ -1,5 +1,27 @@
 ## Give the table format data, and outpout in another format
 
+
+"""
+123  abc
+bcd  345
+abc  345 
+123  hij
+
+==>
+
+123	 abc 	0	1
+bcd	 345	2	3
+abc  345    1	3
+123	 hij    0	4
+
+strs, index
+123    0
+abc    1
+bcd    2
+345    3
+hij    4
+"""
+
 data = '123\tabc\nbcd\t345\nabc\t345\n123\thij\n'
 
 lines = data.split('\n')[:-1]
